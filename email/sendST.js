901443910490bf04677c13d12cc6541b4c9d5ccc
Arturo Sanchez <arturo66cta@gmail.com>
2022-09-12 19:28:04 -0300

    IMPORTANT: finsih version to ST

--- email/sendST.js
+++ email/sendST.js
@@ -2,9 +2,10 @@ const sleep = require('system-sleep');
 const nodemailer = require('nodemailer');
 const fs = require('fs');
 const handlebars = require('handlebars');
+const { title } = require('process');
 // const config  = require('../data/subscriptions_worcap2022.json');
 const { GMAIL_USER, GMAIL_PASS } = process.env;
-const handle = fs.readFileSync('./data/ST.json');
+const handle = fs.readFileSync('./data/test.json');//'./data/ST.json'
 const rows = JSON.parse(handle);
 
 let readHTMLFile = function(path, callback) {
@@ -20,16 +21,18 @@ let readHTMLFile = function(path, callback) {
 };
 
 let sendEmail = function (from, to, bcc, cc, subject, replacements, filepath) {
+    console.log('Sending email to ' + to);
     readHTMLFile(filepath, function(err, html) {
         let template = handlebars.compile(html);
         let htmlToSend = template(replacements);
         let transporter = nodemailer.createTransport({
             service: 'gmail',
             auth: {
-                user: USER,
-                pass: PASS
+                user: GMAIL_USER,
+                pass: GMAIL_PASS
             }
         });
+        
         let mailOptions = {
             from: from,
             to: to,
@@ -38,7 +41,7 @@ let sendEmail = function (from, to, bcc, cc, subject, replacements, filepath) {
             subject: subject,
             html : htmlToSend
         };
-        // console.log(mailOptions);
+
         transporter.sendMail(mailOptions, function(error, info){
             if (error) {
                 console.log(error);
@@ -46,21 +49,22 @@ let sendEmail = function (from, to, bcc, cc, subject, replacements, filepath) {
                 console.log('Email sent: ' + info.response);
             }
         });
+        // console.log('Email sent to ' + to);
     });
 };
 
 for(let row of rows){
-    const { email, name } = row;
+    const { email, name, title } = row;
     console.log(email, name);
-    // sendEmail(
-    //     from="Adriano - WorCAP 2022",
-    //     to=email,
-    //     bcc="uadrianoalmeida@gmail.com",
-    //     cc="uadrianoalmeida@gmail.com",//"worcap@inpe.br",
-    //     subject="WorCAP 2022 - ST",
-    //     replacements={'name': name}, 
-    //     filepath="./data/ST.html"
-    // );
+    sendEmail(
+        from="Arturo - WorCAP 2022",
+        to=email,
+        bcc="uadrianoalmeida@gmail.com",
+        cc="uadrianoalmeida@gmail.com",//"worcap@inpe.br",
+        subject="WorCAP 2022 - ST",
+        replacements={'name': name, 'title': title}, 
+        filepath="./data/ST.html"
+    );
 
     console.log("waiting...");
     sleep(30000); // sleep for 30 seconds
